f608e79c8005283df9b87c8966471a6d3a815bc5
JackLenard <80309855+JackLenard@users.noreply.github.com>
2021-10-04 05:55:10 +1100

    Camera and movement (#3)
    * Implement player movement and isometric camera orientation
    
    * Make camera follow player, add spartan asset
    
    * Add movement animations
    
    * Disable running without movement
    
    * Add default controls
    
    * Remove custom scene
    
    * Add custom scene from master

---
+++ Assets/CameraController.cs
@@ -0,0 +1,27 @@
+// Adapted from ?
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;        //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start () 
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        transform.position = player.transform.position + new Vector3(-11, 10, -12);
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate () 
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+    }
+}
