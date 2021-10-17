dc309f340a858838435ab4a497e33a5d7ea7789d
proguy18 <josephjoey.leonardi@gmail.com>
2021-10-17 16:59:43 +0700

    Added skeleton animations

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -8,16 +8,23 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     public float lookRadius = 10f;
 
+    public float attackRange = 2f;
+
+    public float attackSpeed = 1f;
+    private float attackCooldown = 0f;
+
     public int maxHealth = 100;
     int currentHealth;
     
     Transform target;
     NavMeshAgent agent;
+    private Animator m_Animator; 
 
     void Start()
     {
         target = PlayerManager.instance.player.transform;   
         agent = GetComponent<NavMeshAgent>();
+        m_Animator = gameObject.GetComponent<Animator>();
         // Debug.Log("Enemy is currently " + currentHealth + " health.");
         
     }
@@ -48,10 +55,26 @@ public class EnemyController : MonoBehaviour
     }
     void Update () 
     {
+        
+        if(!m_Animator)
+        {
+            m_Animator  = gameObject.GetComponent<Animator>();
+        }
+        
         float distance = Vector3.Distance(target.position, transform.position);
+        // Debug.Log(agent.velocity);
+        // Attacks target if target is within attack range
+        if (distance <= attackRange)
+        {
+            Attack();
+        }
+
+        // Move to target if target is within look radius
         if (distance <= lookRadius) 
         {
             agent.SetDestination(target.position);
+            m_Animator.SetBool("Run", true);
+
 
             if (distance <= agent.stoppingDistance) 
             {
@@ -61,6 +84,12 @@ public class EnemyController : MonoBehaviour
 
             }
         }
+        // Stop running when enemy is not travelling
+        if (agent.velocity.Equals(new Vector3(0, 0, 0)))
+        {
+            m_Animator.SetBool("Run", false);
+        }
+        attackCooldown -= Time.deltaTime;
     }
 
     void FaceTarget()
@@ -73,5 +102,21 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    void Attack()
+    {
+        if (attackCooldown <= 0f)
+        {
+            // Play attack animation
+            m_Animator.SetTrigger("Attack");
+
+            // Check if collides player
+
+            // Damage player
+
+            // Reset attackCooldown
+            attackCooldown = 1 / attackSpeed;
+        }
     }
 }
