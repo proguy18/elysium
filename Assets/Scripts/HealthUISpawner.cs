6ae2a0aae63500b88978e4e93efdebaa08f1b4c0
proguy18 <josephjoey.leonardi@gmail.com>
2021-10-25 01:37:48 +0700

    Refactored code, added health bar, added death animation, added player taking damage

--- Assets/Scripts/HealthUISpawner.cs
+++ Assets/Scripts/HealthUISpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,14 @@ public class HealthUI : MonoBehaviour
     public GameObject uiPrefab;
     public Transform target;
 
-    Transform ui;
-    Image healthSlider;
+    private Transform ui;
+    private Image healthSlider;
+    private Transform cam;
 
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main.transform;
         foreach (Canvas c in FindObjectsOfType<Canvas>())
         {
             if(c.renderMode == RenderMode.WorldSpace) 
@@ -27,7 +30,15 @@ public class HealthUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        
         ui.position = target.position;
         
+    }
+
+    private void LateUpdate()
+    {
+        var camPosition = cam.position;
+        transform.LookAt (new Vector3(camPosition.x,transform.position.y,camPosition.z), Vector3.down);
     }
 }
