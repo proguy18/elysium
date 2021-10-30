d8e879df146468443d6ea61f09a7858ac1f87c27
proguy18 <josephjoey.leonardi@gmail.com>
2021-10-31 00:55:27 +0700

    Fixed UI Animations not playing when game is 'paused'

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -11,27 +11,40 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            LoadNextLevel();
-        }
+        // if (Input.GetKeyDown(KeyCode.L))
+        // {
+        //     LoadNextLevel();
+        // }
     }
 
-    public void LoadNextLevel()
-    {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-    }
+    // public void LoadNextLevel()
+    // {
+    //     StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    // }
 
-    IEnumerator LoadLevel(int levelIndex)
+    // IEnumerator LoadLevel(int levelIndex)
+    // {
+    //     // Play animation
+    //     transition.SetTrigger("Start");
+    //
+    //     // Wait
+    //     yield return new WaitForSeconds(transitionTime);
+    //
+    //     // Load Scene
+    //     SceneManager.LoadScene(levelIndex);
+    // }
+    IEnumerator LoadScene(string sceneName)
     {
         // Play animation
         transition.SetTrigger("Start");
-
+    
         // Wait
-        yield return new WaitForSeconds(transitionTime);
-
+        yield return new WaitForSecondsRealtime(transitionTime);
+        Debug.Log("Scene being loaded...");
+    
         // Load Scene
-        SceneManager.LoadScene(levelIndex);
+        // Invoke("LoadMyScene",2)
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
     public void QuitGame()
     {
@@ -40,17 +53,39 @@ public class LevelLoader : MonoBehaviour
 
     public void GoToMainMenu()
     {
+        
         Debug.Log("Pressed go to main menu");
+        // Play animation
+        // transition.SetTrigger("Start");
+        // Invoke("LoadMainMenu", transitionTime);
         // StartCoroutine(LoadLevel(0));
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
-        // SceneManager.LoadScene("StartScene");
+        // StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
+        // SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+        StartCoroutine(LoadScene("StartScene"));
     }
 
-    public void RestartGame()
+    // private void LoadMainMenu()
+    // {
+    //     Debug.Log("Scene being loaded...");
+    //     SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+    // }
+
+    public void StartGame()
     {
         Debug.Log("Pressed play again");
+        // Play animation
+        // transition.SetTrigger("Start");
+
+        // Invoke("LoadGame", transitionTime);
         // StartCoroutine(LoadLevel(1));
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex));
-        // SceneManager.LoadScene("IntegratedScene");
+        // StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex));
+        // SceneManager.LoadScene("IntegratedScene", LoadSceneMode.Single);
+        StartCoroutine(LoadScene("IntegratedScene"));
     }
+
+    // private void LoadGame()
+    // {
+    //     Debug.Log("Scene being loaded...");
+    //     SceneManager.LoadScene("IntegratedScene", LoadSceneMode.Single);
+    // }
 }
